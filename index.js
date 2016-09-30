405b7a1061eec8619489d44143b12992d8a0ea15
haobing <hobart.cn@gmail.com>
2016-09-30 22:58:48 +0800

    npm run lint

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function add(arg1, arg2) {
 
     let r1, r2, m;
     try {
-        r1 = num1.toString().split(".")[1].length;
+        r1 = num1.toString().split('.')[1].length;
     } catch (e) {
         r1 = 0;
     }
 
     try {
-        r2 = num2.toString().split(".")[1].length;
+        r2 = num2.toString().split('.')[1].length;
     } catch (e) {
         r2 = 0;
     }
@@ -27,7 +27,7 @@ function add(arg1, arg2) {
     m = Math.pow(10, Math.max(r1, r2));
 
     return (num1 * m + num2 * m) / m;
-};
+}
 
 /**
  * 精确减法
@@ -42,14 +42,14 @@ function sub(arg1, arg2) {
     const num1 = parseFloat(arg1);
     const num2 = parseFloat(arg2);
 
-    let r1, r2, m, n;
+    let r1, r2, m;
     try {
-        r1 = num1.toString().split(".")[1].length;
+        r1 = num1.toString().split('.')[1].length;
     } catch (e) {
         r1 = 0;
     }
     try {
-        r2 = num2.toString().split(".")[1].length;
+        r2 = num2.toString().split('.')[1].length;
     } catch (e) {
         r2 = 0;
     }
@@ -57,7 +57,7 @@ function sub(arg1, arg2) {
     m = Math.pow(10, Math.max(r1, r2));
 
     return ((num1 * m - num2 * m) / m);
-};
+}
 
 /**
  * 精确乘法
@@ -73,15 +73,17 @@ function mul(arg1, arg2) {
 
     let m = 0, s1 = num1.toString(), s2 = num2.toString();
     try {
-        m += s1.split(".")[1].length;
+        m += s1.split('.')[1].length;
     } catch (e) {
+        //nothing
     }
     try {
-        m += s2.split(".")[1].length;
+        m += s2.split('.')[1].length;
     } catch (e) {
+        //nothing
     }
-    return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
-};
+    return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m);
+}
 
 
 /**
@@ -99,18 +101,20 @@ function div(arg1, arg2) {
     let t1 = 0, t2 = 0, r1, r2;
 
     try {
-        t1 = num1.toString().split(".")[1].length;
+        t1 = num1.toString().split('.')[1].length;
     } catch (e) {
+        //nothing
     }
 
     try {
-        t2 = num2.toString().split(".")[1].length;
+        t2 = num2.toString().split('.')[1].length;
     } catch (e) {
+        //nothing
     }
 
-    r1 = Number(num1.toString().replace(".", ""));
-    r2 = Number(num2.toString().replace(".", ""));
+    r1 = Number(num1.toString().replace('.', ''));
+    r2 = Number(num2.toString().replace('.', ''));
     return (r1 / r2) * Math.pow(10, t2 - t1);
-};
+}
 
 export {add, sub, mul, div };
